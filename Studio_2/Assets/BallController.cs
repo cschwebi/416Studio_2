db7dbd32248260b2a5598d2b40d8688035f7a4df
Charlie Schwebius <cschwebi@student.ubc.ca>
2025-02-15 16:19:09 -0800

    added pins, ball, and early movement (up to step 2.1)

---
+++ Studio_2/Assets/BallController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+private bool isBallLaunched;
+[SerializeField] private float force = 1f;
+[SerializeField] private InputManager inputManager;
+private Rigidbody ballRB;
+    void Start()
+    {
+        ballRB = GetComponent<Rigidbody>();
+
+// Add a listener to the OnSpacePressed event.
+// When the space key is pressed the
+// LaunchBall method will be called.
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall()
+    {
+        if(isBallLaunched) return;
+
+        isBallLaunched = true;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+}
